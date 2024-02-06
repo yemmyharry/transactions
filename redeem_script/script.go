2e46afef2ee84463a5ca6292c913030833cb5e66
yemmyharry <yemmyharry@gmail.com>
2024-02-06 10:49:01 +0100

    added get redeem script function

---
+++ redeem_script/script.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+func GetRedeemScript(preimage string) string {
+	// Encode the preimage
+	preimageBytes := []byte(preimage)
+
+	// Compute the SHA-256 hash
+	hasher := sha256.New()
+	hasher.Write(preimageBytes)
+	hashBytes := hasher.Sum(nil)
+
+	// Encode the hash to hex
+	hashHex := hex.EncodeToString(hashBytes)
+	//OP_SHA256 = 0xa8
+	//OP_EQUAL= 87
+	redeemScript := "a8" + hashHex + "88"
+
+	fmt.Println("Redeem script (hex):", redeemScript)
+
+	return redeemScript
+}
